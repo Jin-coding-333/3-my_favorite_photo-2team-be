4ac7017461c593956a5819406a8b0ec2ebad7704
kimhyunmook_mac <gusanr4200@naver.com>
2024-12-31 09:19:00 +0900

    .

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ import users from "./users/controller.js";
 
 const router = express.Router();
 
-/** 여기에 Router 추가해주세요 */
+/** 여기에 Router 추가해주세요. */
 router.use("/users", users);
 
 export default router;
