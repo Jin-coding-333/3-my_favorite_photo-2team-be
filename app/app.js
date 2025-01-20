eb9edef5f8a054841896bea492f0f44fb22190ae
Kimhyunmook <gusanr4200@naver.com>
2025-01-20 23:02:03 +0900

    options add

--- app/app.js
+++ app/app.js
@@ -11,12 +11,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://my-favorite-photo.onrender.com"],
+    origin: ORIGIN,
+    // origin: ["http://localhost:3000", "https://my-favorite-photo.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
-
+app.options("*", cors());
 app.use("/api", router);
 
 app.listen(PORT, () => {
