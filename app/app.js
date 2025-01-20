fd4711b560c9260d2bbc2003f36c57f9cac2be1f
Kimhyunmook <gusanr4200@naver.com>
2025-01-20 22:36:03 +0900

    cors

--- app/app.js
+++ app/app.js
@@ -13,6 +13,7 @@ app.use(
   cors({
     origin: ["http://localhost:3000", "https://my-favorite-photo.onrender.com"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
 
